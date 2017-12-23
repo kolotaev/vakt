2d1f2b62c016b43e57aae2461e36e73ddc24e067
kolotaev <kolotaev@tut.by>
2017-12-24 00:58:26 +0300

    Add docs to manager

--- manager.py
+++ manager.py
@@ -1,23 +1,34 @@
 from abc import ABC, abstractmethod
 
 
-class IManager(ABC):
+class PolicyManager(ABC):
+    """
+    Interface for managers that persist policies.
+    Every manager should implement all the specified methods, but how, it's up to it to decide:
+    it can be in-memory storage, SQL database, NoSQL solution, etc.
+    """
+
     @abstractmethod
     def get(self, id):
+        """Retrieve specific policy"""
         pass
 
     @abstractmethod
     def get_all(self, limit, offset):
+        """Retrieve all the policies within a window"""
         pass
 
     @abstractmethod
     def create(self, policy):
+        """Create a policy"""
         pass
 
     @abstractmethod
     def update(self, policy):
+        """Update a policy"""
         pass
 
     @abstractmethod
     def delete(self, id):
+        """Delete a policy"""
         pass
