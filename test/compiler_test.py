6de6ebe48471b2c1282ea5e9f725e1be557107e9
kolotaev <kolotaev@tut.by>
2018-01-01 23:57:45 +0300

    Add test-cases to compiler

--- test/compiler_test.py
+++ test/compiler_test.py
@@ -5,7 +5,9 @@ from vakt.compiler import compile_regex
 @pytest.mark.parametrize('phrase, start, end, output', [
     ('foo:bar:<.*>', '<', '>', '^foo\\:bar\\:(.*)$'),
     ('foo:bar:[.*]', '[', ']', '^foo\\:bar\\:(.*)$'),
+    ('foo:bar:[фу-бар.*]', '[', ']', '^foo\\:bar\\:(фу-бар.*)$'),
     ('a', '{', '}', '^a$'),
+    ('', '{', '}', '^$'),
     ('a:b:{foo-bar.*i{2}}', '{', '}', '^a\\:b\\:(foo-bar.*i{2})$')
 ])
 def test_compile_regex_compiles_correctly(phrase, start, end, output):
