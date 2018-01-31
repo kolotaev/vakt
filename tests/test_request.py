08ab8627c863deaa8128644880ebca6d3433b356
kolotaev <kolotaev@tut.by>
2018-01-31 21:32:50 +0300

    Add request test

---
+++ tests/test_request.py
@@ -0,0 +1,11 @@
+from vakt.guard import Request
+
+
+def test_json_roundtrip_of_request():
+    r = Request(resource='books:abc', action='view', subject='bobby', context={'ip': '127.0.0.1'})
+    s = r.to_json()
+    r1 = Request.from_json(s)
+    assert 'books:abc' == r1.resource
+    assert 'view' == r1.action
+    assert 'bobby' == r1.subject
+    assert {'ip': '127.0.0.1'} == r1.context
