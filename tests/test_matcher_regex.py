e3245668f225ff030c9d88a5557cc0bea588bee3
kolotaev <kolotaev@tut.by>
2018-02-01 16:21:52 +0300

    Start RegexMatcher tests

---
+++ tests/test_matcher_regex.py
@@ -0,0 +1,20 @@
+# import pytest
+#
+# from vakt.matcher import RegexMatcher
+# from vakt.policy import DefaultPolicy
+#
+#
+# @pytest.mark.parametrize('policy, field, what, result', [
+#     (DefaultPolicy('1', actions=['get']), 'actions', 'get', True),
+#     (DefaultPolicy('1', actions=['get', 'list']), 'actions', 'get', True),
+#     (DefaultPolicy('1', actions=['get', 'list']), 'actions', 'list', True),
+#     (DefaultPolicy('1', actions=['get', 'list']), 'non_existing_field', 'get', False),
+#     (DefaultPolicy('1', actions=['<get>']), 'actions', 'get', True),
+#     (DefaultPolicy('1', actions=['<get>']), 'actions', '<get>', False),
+#     (DefaultPolicy('1', actions=['<get']), 'actions', 'get', False),
+#     (DefaultPolicy('1', resources=['books:1', 'books:2']), 'resources', 'books:3', False),
+#     (DefaultPolicy('1', resources=['books:1', 'books:2']), 'resources', 'books:1', True),
+# ])
+# def test_matches(policy, field, what, result):
+#     m = RegexMatcher()
+#     assert result == m.matches(policy, field, what)
