71bfde757769e667f203dae0524fbd54704a42d4
kolotaev <kolotaev@tut.by>
2018-12-10 13:41:03 +0300

    Add type to Inquiry. + Unit tests

---
+++ tests/test_vakr_init.py
@@ -0,0 +1,6 @@
+from vakt import TYPE_ATTRIBUTES, TYPE_STRINGS
+
+
+def test_types():
+    assert 1 == TYPE_STRINGS
+    assert 2 == TYPE_ATTRIBUTES
--- vakt/guard.py
+++ vakt/guard.py
@@ -6,7 +6,7 @@ Also contains Inquiry class.
 import logging
 
 from .util import JsonSerializer, PrettyPrint
-# from . import TYPE_STRINGS, TYPE_ATTRIBUTES
+from . import TYPE_STRINGS, TYPE_ATTRIBUTES
 
 
 log = logging.getLogger(__name__)
@@ -22,14 +22,16 @@ class Inquiry(JsonSerializer, PrettyPrint):
         self.action = action or ''
         self.subject = subject or ''
         self.context = context or {}
-        # if any([isinstance(a, dict) for a in [self.resource, self.action, self.subject]]):
-        #     self.type = TYPE_ATTRIBUTES
-        # else:
-        #     self.type = TYPE_STRINGS
+        if any([isinstance(a, dict) for a in [self.resource, self.action, self.subject]]):
+            self.type = TYPE_ATTRIBUTES
+        else:
+            self.type = TYPE_STRINGS
 
     @classmethod
     def from_json(cls, data):
         props = cls._parse(data)
+        if 'type' in props:
+            del props['type']
         return cls(**props)
 
 
