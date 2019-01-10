f99e07f02c7fc9d502ca3dbee10f7562ec9be1ce
kolotaev <kolotaev@tut.by>
2019-01-10 23:12:21 +0300

    Fix versions of dev deps

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
         ],
         extras_require={
             'dev': [
-                'pytest>=3.0.0',
-                'pytest-cov',
+                'pytest~=3.0',
+                'pytest-cov~=2.9',
                 'pylint',
             ],
             'mongo': [
