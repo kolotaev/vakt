4071fcfc9263078a1239159b027ee05ecacabfc4
kolotaev <kolotaev@tut.by>
2019-06-28 12:06:35 +0300

    Use load_all for multi-documents

--- tests/readers/test_yaml_reader.py
+++ tests/readers/test_yaml_reader.py
@@ -1,7 +1,8 @@
-from yaml import load, dump, Loader, Dumper
+from yaml import load, load_all, Loader
 
 
 def test_example_yaml():
     with open('vakt/examples/read-policies-from-file/policies.yaml', 'r') as f:
-        data = load(f, Loader=Loader)
+        # use load-all for multi-documents
+        data = load_all(f, Loader=Loader)
         assert data
