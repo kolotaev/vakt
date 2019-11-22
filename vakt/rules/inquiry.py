14a2da0dd241667a56e55665af16ed32f5eb5332
kolotaev <kolotaev@tut.by>
2019-11-23 00:35:43 +0300

    [Rules] initial implementation of attributes-aware inquiry rules

--- vakt/rules/inquiry.py
+++ vakt/rules/inquiry.py
@@ -2,21 +2,69 @@
 All Rules relevant to Inquiry context
 """
 import warnings
+from abc import ABCMeta, abstractmethod
 
 from ..rules.base import Rule
+from ..rules import Eq
 
 __all__ = [
     'SubjectEqual',
     'ActionEqual',
     'ResourceIn',
+    'InquirySubjectMatch',
+    'InquiryActionMatch',
+    'InquiryResourceMatch',
 ]
+
+
+class InquiryMatch(Rule, metaclass=ABCMeta):
+    def __init__(self, attribute=None, rule_class=Eq):
+        self.attribute = attribute
+        self.rule_class = rule_class.__class__.__name__
+
+    def satisfied(self, what, inquiry=None):
+        if not inquiry:
+            return False
+        inquiry_value = getattr(inquiry, self._field_name())
+        if self.attribute is not None:
+            if isinstance(inquiry_value, dict) and self.attribute in inquiry_value:
+                inquiry_value = inquiry_value[self.attribute]
+            else:
+                return False
+        try:
+            rule = self.rule_class.__new__(*inquiry_value)
+            return rule.satisfied(what)
+        except:
+            return False
+
+    @abstractmethod
+    def _field_name(self):
+        pass
+
+
+class InquirySubjectMatch:
+    @abstractmethod
+    def _field_name(self):
+        return 'subject'
+
+
+class InquiryActionMatch:
+    @abstractmethod
+    def _field_name(self):
+        return 'action'
+
+
+class InquiryResourceMatch:
+    @abstractmethod
+    def _field_name(self):
+        return 'resource'
 
 
 class SubjectEqual(Rule):
     """
     Rule that is satisfied if the string value equals the Inquiry's Subject.
     For example: context={'user': SubjectEqual()}
-    This is an old Rule. Most often operator Rules will be enough.
+    This Rule only works for string-based policies.
     """
     def satisfied(self, what, inquiry=None):
         return inquiry and isinstance(what, str) and what == inquiry.subject
@@ -26,7 +74,7 @@ class ActionEqual(Rule):
     """
     Rule that is satisfied if the string value equals the Inquiry's Action.
     For example: context={'user': ActionEqual()}
-    This is an old Rule. Most often operator Rules will be enough.
+    This Rule only works for string-based policies.
     """
     def satisfied(self, what, inquiry=None):
         return inquiry and isinstance(what, str) and what == inquiry.action
@@ -36,7 +84,7 @@ class ResourceIn(Rule):
     """
     Rule that is satisfied if list contains the Inquiry's Resource.
     For example: context={'user': ResourceIn()}
-    This is an old Rule. Most often list Rules will be enough.
+    This Rule only works for string-based policies.
     """
     def satisfied(self, what, inquiry=None):
         return inquiry and isinstance(what, list) and inquiry.resource in what
