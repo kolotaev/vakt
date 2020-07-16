b3f2c85d95eec589c1950e1c6f42a60439f75ec0
kolotaev <kolotaev@tut.by>
2020-07-16 10:09:59 +0300

    [audit] Add basic audit tests

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -59,17 +59,51 @@ def test_policies_count_msg():
     assert 'count = 0' == str(m)
 
 
-def test_formatter():
-    log_capture_string = io.StringIO()
-    h = logging.StreamHandler(log_capture_string)
+def test_formatter_usage():
+    log_capture_str = io.StringIO()
+    h = logging.StreamHandler(log_capture_str)
     h.setLevel(logging.DEBUG)
     h.setFormatter(logging.Formatter(
         '%(name)s - level: %(levelname)s - msg: %(message)s - effect: %(effect)s - pols: %(policies)s'
     ))
     audit_log = logging.getLogger('vakt.audit')
+    initial_level = audit_log.getEffectiveLevel()
     audit_log.setLevel(logging.DEBUG)
     audit_log.addHandler(h)
     pmc = PoliciesUidMsg
-    audit_log.info('Test', extra={'effect': ALLOW_ACCESS, 'policies': pmc([Policy(123), Policy('asdf')])})
-    assert 'vakt.audit - level: INFO - msg: Test - effect: allow - pols: [123, asdf]\n' == \
-           log_capture_string.getvalue()
+    try:
+        audit_log.info('Test', extra={'effect': ALLOW_ACCESS, 'policies': pmc([Policy(123), Policy('asdf')])})
+        assert 'vakt.audit - level: INFO - msg: Test - effect: allow - pols: [123, asdf]\n' == \
+               log_capture_str.getvalue()
+    finally:
+        audit_log.removeHandler(h)
+        audit_log.setLevel(initial_level)
+
+
+def test_audit_logs_messages_at_info_level():
+    log_capture_str = io.StringIO()
+    h = logging.StreamHandler(log_capture_str)
+    h.setLevel(logging.INFO)
+    audit_log = logging.getLogger('vakt.audit')
+    initial_level = audit_log.getEffectiveLevel()
+    audit_log.setLevel(logging.INFO)
+    audit_log.addHandler(h)
+    try:
+        audit_log.info('Test')
+        assert 'Test\n' == log_capture_str.getvalue()
+    finally:
+        audit_log.removeHandler(h)
+        audit_log.setLevel(initial_level)
+    # Let's decrease verbosity
+    log_capture_str = io.StringIO()
+    h = logging.StreamHandler(log_capture_str)
+    h.setLevel(logging.WARN)
+    audit_log = logging.getLogger('vakt.audit')
+    audit_log.setLevel(logging.WARN)
+    audit_log.addHandler(h)
+    try:
+        audit_log.info('Test')
+        assert '' == log_capture_str.getvalue()
+    finally:
+        audit_log.removeHandler(h)
+        audit_log.setLevel(initial_level)
