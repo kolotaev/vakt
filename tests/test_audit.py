fd6eb10a67cab58500e8648d73c7e42651a8bc29
kolotaev <kolotaev@tut.by>
2020-07-16 09:34:33 +0300

    [audit] Add policy message classes tests

--- tests/test_audit.py
+++ tests/test_audit.py
@@ -1,9 +1,62 @@
 import logging
 import io
 
-from vakt.audit import PoliciesUidMsg
+from vakt.audit import (PoliciesUidMsg, PoliciesNopMsg,
+                        PoliciesDescriptionMsg, PoliciesCountMsg)
 from vakt.policy import Policy
 from vakt.effects import ALLOW_ACCESS
+
+
+def test_policies_nop_msg():
+    p1 = Policy(123, description='foo', actions=['books:<foo.bar>'],
+                resources=['bb'], subjects=['<qwerty>'], context={})
+    p2 = Policy('asdf', description='bar', actions=['books:<foo.bar>'],
+                resources=['aa'], subjects=['<qwerty>'], context={})
+    m = PoliciesNopMsg([p1, p2])
+    assert '' == str(m)
+    m = PoliciesNopMsg([p2])
+    assert '' == str(m)
+    m = PoliciesNopMsg()
+    assert '' == str(m)
+
+
+def test_policies_uid_msg():
+    p1 = Policy(123, description='foo', actions=['books:<foo.bar>'],
+                resources=['bb'], subjects=['<qwerty>'], context={})
+    p2 = Policy('asdf', description='bar', actions=['books:<foo.bar>'],
+                resources=['aa'], subjects=['<qwerty>'], context={})
+    m = PoliciesUidMsg([p1, p2])
+    assert '[123, asdf]' == str(m)
+    m = PoliciesUidMsg([p2])
+    assert '[asdf]' == str(m)
+    m = PoliciesUidMsg()
+    assert '[]' == str(m)
+
+
+def test_policies_description_msg():
+    p1 = Policy(123, description='foo', actions=['books:<foo.bar>'],
+                resources=['bb'], subjects=['<qwerty>'], context={})
+    p2 = Policy('asdf', description='bar', actions=['books:<foo.bar>'],
+                resources=['aa'], subjects=['<qwerty>'], context={})
+    m = PoliciesDescriptionMsg([p1, p2])
+    assert "['foo', 'bar']" == str(m)
+    m = PoliciesDescriptionMsg([p2])
+    assert "['bar']" == str(m)
+    m = PoliciesDescriptionMsg()
+    assert '[]' == str(m)
+
+
+def test_policies_count_msg():
+    p1 = Policy(123, description='foo', actions=['books:<foo.bar>'],
+                resources=['bb'], subjects=['<qwerty>'], context={})
+    p2 = Policy('asdf', description='bar', actions=['books:<foo.bar>'],
+                resources=['aa'], subjects=['<qwerty>'], context={})
+    m = PoliciesCountMsg([p1, p2])
+    assert 'count = 2' == str(m)
+    m = PoliciesCountMsg([p2])
+    assert 'count = 1' == str(m)
+    m = PoliciesCountMsg()
+    assert 'count = 0' == str(m)
 
 
 def test_formatter():
@@ -18,5 +71,5 @@ def test_formatter():
     audit_log.addHandler(h)
     pmc = PoliciesUidMsg
     audit_log.info('Test', extra={'effect': ALLOW_ACCESS, 'policies': pmc([Policy(123), Policy('asdf')])})
-    assert 'vakt.audit - level: INFO - msg: Test - effect: allow - pols: [123,asdf]\n' == \
+    assert 'vakt.audit - level: INFO - msg: Test - effect: allow - pols: [123, asdf]\n' == \
            log_capture_string.getvalue()
