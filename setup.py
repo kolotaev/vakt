bdb06ae205a5ed4bfb604967e2b2ab149558760b
kolotaev <kolotaev@tut.by>
2020-07-23 18:40:27 +0300

    [redis] start

--- setup.py
+++ setup.py
@@ -42,6 +42,9 @@ if __name__ == '__main__':
             'sql': [
                 'SQLAlchemy~=1.3'
             ],
+            'redis': [
+                'redis~=3.5'
+            ],
         },
         packages=find_packages(exclude='tests'),
         classifiers=[
---
+++ vakt/storage/redis.py
@@ -0,0 +1,49 @@
+"""
+Redis storage for Policies.
+"""
+
+import logging
+
+from ..storage.abc import Storage
+
+
+log = logging.getLogger(__name__)
+
+
+class RedisStorage(Storage):
+    """Stores all policies in Redis"""
+
+    def __init__(self):
+        pass
+
+    def add(self, policy):
+        uid = policy.uid
+        with self.lock:
+            if uid in self.policies:
+                log.error('Error trying to create already existing policy with UID=%s', uid)
+                raise PolicyExistsError(uid)
+            self.policies[uid] = policy
+            log.info('Added Policy: %s', policy)
+
+    def get(self, uid):
+        return self.policies.get(uid)
+
+    def get_all(self, limit, offset):
+        self._check_limit_and_offset(limit, offset)
+        result = [v for v in self.policies.values()]
+        if offset > len(result) or limit == 0:
+            return []
+        return result[offset:limit+offset]
+
+    def find_for_inquiry(self, inquiry, checker=None):
+        with self.lock:
+            return self.policies.values()
+
+    def update(self, policy):
+        self.policies[policy.uid] = policy
+        log.info('Updated Policy with UID=%s. New value is: %s', policy.uid, policy)
+
+    def delete(self, uid):
+        if uid in self.policies:
+            del self.policies[uid]
+            log.info('Policy with UID %s was deleted', uid)
