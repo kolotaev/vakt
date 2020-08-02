a6fa0b97d182b43345f9b5a9178b40ddaa013188
kolotaev <kolotaev@tut.by>
2020-08-02 16:13:29 +0300

    [file] try to read policies from file in the FileStorage

--- vakt/storage/file.py
+++ vakt/storage/file.py
@@ -6,6 +6,7 @@ import logging
 
 from ..storage.abc import Storage
 from ..storage.memory import MemoryStorage
+from ..readers import JSONReader, YamlReader
 
 
 log = logging.getLogger(__name__)
@@ -15,10 +16,23 @@ class FileStorage(Storage):
     """
     Reads all policies from file and keeps them in the backing Storage (in memory by default).
     """
-
     def __init__(self, file, storage=None):
         if storage is None:
             self.back_store = MemoryStorage()
+        read_done = []
+        reader = None
+        readers = [JSONReader, YamlReader]
+        while not len(read_done) < len(readers):
+            try:
+                for reader_cls in readers:
+                    reader = reader_cls(file)
+                    read_done.append(True)
+            except Exception:
+                read_done.append(False)
+        if not reader:
+            raise RuntimeError('Failed to created reader from file %s', file)
+        for p in reader.read():
+            self.back_store.add(p)
 
     def add(self, policy):
         raise NotImplementedError('Please, add Policy in file manually')
